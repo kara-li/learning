ae55c139acf550ed4fae0b343f075be33470f783
Kara Li <likara@seas.upenn.edu>
2020-08-24 13:38:48 -0400

    continued js crash course, about halfway through

--- javascript/main.js
+++ javascript/main.js
@@ -64,3 +64,154 @@ person.email = "kara@gmail.com";
 console.log(person);
 
 //arrays of objects
+const todos = [
+  {
+    id: 1,
+    text: "Take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Meeting with boss",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Dentist appt",
+    isCompleted: false,
+  },
+];
+
+console.log(todos);
+console.log(todos[2].text);
+
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
+
+// loops
+//for
+for (let i = 0; i < 10; i++) {
+  console.log(`for loop number ${i}`);
+}
+
+//while
+let i = 0;
+while (i < 10) {
+  console.log(`while loop number ${i}`);
+  i++;
+}
+
+//array loops
+for (let i = 0; i < todos.length; i++) {
+  console.log(todos[i]);
+}
+
+//for of
+for (let todo of todos) {
+  //todo can be named anything, todos is name of array
+  console.log(todo.text);
+}
+
+//high order array methods - suggested way for array iteration
+//forEach, map. filter
+todos.forEach(function (todo) {
+  console.log(todo.text);
+});
+
+const todoText = todos.map(function (todo) {
+  //.map returns an array
+  return todo.text;
+});
+
+console.log(todoText);
+
+const todoCompleted = todos.filter(function (todo) {
+  return todo.isCompleted === true;
+});
+
+console.log(todoCompleted);
+
+const todoCompletedText = todos
+  .filter(function (todo) {
+    return todo.isCompleted === true;
+  })
+  .map(function (todo) {
+    return todo.text;
+  });
+
+console.log(todoCompletedText);
+
+//conditionals
+const x = "10";
+const y = 10;
+if (x == 10 || y > 10) {
+  //&& for and
+  //will be true
+  //double equals doesn't match data type, only value
+  //triple equals also matches data types - mostly always use triple equals bc you normally want to match types
+  console.log("x is 10");
+} else if (x > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is less than 10");
+}
+
+//ternary operator - used a lot to assign variable based on condition
+const w = 10;
+//? is like "then" (its the ternary operator); : is "else"
+const color = w > 10 ? "red" : "blue";
+console.log(color);
+
+//switches
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  default:
+    console.log("color is not red or blue");
+    break;
+}
+
+//functions
+function addNums(num1, num2) {
+  console.log(num1 + num2);
+}
+addNums(5, 4);
+
+const addNumsWithDefaultValues = (num1 = 1, num2 = 1) => {
+  //fat arrow
+  console.log(num1 + num2);
+  //usuallly return the value and if want to log do it outside
+  //return num1 + num2
+};
+//if wanted to return smtg, or not multiline, don't need {}
+//const someFunction = num1 => num1 + 5; //if put return, wouldnt' work, don't need paranthesees if only 1 parameter
+addNumsWithDefaultValues();
+addNumsWithDefaultValues(5, 5); //will override default values
+
+todos.forEach((todo) => console.log(todo));
+
+//constructor function should start w/ capital, pass in properties you wnat to be able to set
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
+  this.getBirthYear = function () {
+    return this.dob.getFullYear();
+  };
+  this.getFullName = function () {
+    return `${firstName} ${lastName}`;
+  };
+}
+
+//instantiate object
+const person1 = new Person("Kara", "Li", "4-3-1900");
+const person2 = new Person("John", "Doe", "5-4-1901");
+console.log(person1); //will look like object literal but is prefixed w/ name of object (Person)
+console.log(person2.dob);
+console.log(person2.dob.getFullYear());
+console.log(person2.getBirthYear());
+console.log(person1.getFullName());
